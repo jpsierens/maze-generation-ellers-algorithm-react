ebe1b025b1a30a79ff5b3b12f19541ca1f46d74b
jpsierens <jpsierens1988@gmail.com>
2019-12-27 10:30:19 -0600

    adding 1 row

--- index.js
+++ index.js
@@ -4,30 +4,50 @@ import { render } from 'react-dom';
 import Cell from './Cell.js';
 import './style.css';
 
-class App extends Component {
+class Maze extends Component {
   constructor() {
     super();
-    this.state = {
-      name: 'React'
-    };
-  }
-
-  render() {
-    const walls = {
+    this.speed = 1000;
+    this.initialWalls = {
       left: true,
       right: true,
       top: true,
       bottom: true,
     }
+    this.state = {
+      width: 10,
+      height: 10,
+      cells: [],
+    };
+  }
+
+  componentDidMount() {
+    this.timerID = setInterval(
+      () => this.tick(),
+      this.speed
+    );
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    this.setState({
+      cells: this.state.cells.push(<Cell walls={this.initialWalls}/>)
+    });
+  }
+
+  render() {
     return (
       <div>
         <p>
           Eller's Algorithm for Maze Generation in React
         </p>
-        <Cell walls={walls}/>
+        {this.state.cells}
       </div>
     );
   }
 }
 
-render(<App />, document.getElementById('root'));
+render(<Maze />, document.getElementById('root'));
