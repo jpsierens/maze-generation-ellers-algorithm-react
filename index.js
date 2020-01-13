84973d661d6d389252d3cc87fc87e6e75a30bf75
jpsierens <jpsierens1988@gmail.com>
2020-01-13 17:08:02 -0600

    extract Row component

--- index.js
+++ index.js
@@ -1,119 +1,12 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
 
-import Cell from './Cell.js';
+import Row from './Row.js';
 import './style.css';
 
 class Maze extends Component {
   constructor() {
     super();
-    this.currentCell = 0;
-    this.timesTicked = 0;
-    this.speed = 500;
-    this.chanceToJoin = 0.3; // x100 for %
-    this.width = 10;
-    this.height = 10;
-    this.initialWalls = {
-      left: true,
-      right: true,
-      top: true,
-      bottom: true,
-    }
-    this.state = {
-      cells: [],
-    };
-  }
-
-  componentDidMount() {
-    this.timerID = setInterval(
-      () => this.tick(),
-      this.speed
-    );
-  }
-
-  componentWillUnmount() {
-    clearInterval(this.timerID);
-  }
-
-  createRow() {
-    this.setState({
-      cells: [
-        ...this.state.cells, 
-        <Cell setID={this.currentCell}
-              walls={this.initialWalls}/>
-      ]
-    });
-    this.currentCell += 1;
-  }
-
-  willJoin() {
-    return Math.random() > this.chanceToJoin;
-  }
-
-  getCellsAndHighlightCurrent() {
-    return this.state.cells.map((cell, i) => {
-      if (i !== this.currentCell) {
-        return <Cell setID={cell.props.setID}
-                     active={false}
-                     walls={cell.props.walls}/>
-      }
-      return cell;
-    });
-  }
-
-  joinCellToSet(cells, currentCellSetId) {
-    const walls = {
-      ...cells[this.currentCell].props.walls,
-      right: false,
-    }
-    return (
-      <Cell setID={currentCellSetId}
-            active={true}
-            walls={walls}/>
-    );
-  }
-
-  joinCellToLastSet(cells) {
-    const walls = {
-      ...cells[this.currentCell].props.walls,
-      left: false,
-    }
-    return (
-      <Cell setID={cells[this.currentCell - 1].props.setID}
-            active={true}
-            walls={walls}/>
-    );
-  }
-
-  joinSomeCells() {
-    if (this.currentCell > this.width - 1) {
-      clearInterval(this.timerID);
-      return;
-    }
-
-    const cells = this.getCellsAndHighlightCurrent();
-    const currentCellSetId = cells[this.currentCell].props.setID;
-
-    if (this.willJoin()) {
-      cells[this.currentCell] = this.joinCellToSet(cells, currentCellSetId);
-    }
-
-    if (cells[this.currentCell - 1] && !cells[this.currentCell - 1].props.walls.right) {
-      cells[this.currentCell] = this.joinCellToLastSet(cells);
-    }
-
-    this.setState({ cells });
-  }
-
-  tick() {
-    this.timesTicked++;
-    console.log(this.timesTicked);
-    if (this.state.cells.length >= this.width) {
-      this.currentCell = this.timesTicked - this.width - 1;
-      this.joinSomeCells();
-      return;
-    }
-    this.createRow();
   }
 
   render() {
@@ -122,7 +15,7 @@ class Maze extends Component {
         <p>
           Eller's Algorithm for Maze Generation in React
         </p>
-        {this.state.cells}
+        <Row />
       </div>
     );
   }
